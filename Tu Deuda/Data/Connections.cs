8f8e496a648e590f825672bfa8df546100845286
theerudito <erudito.tv@gmail.com>
2023-05-15 14:44:35 -0500

    v5

---
+++ Tu Deuda/Data/Connections.cs
@@ -0,0 +1,38 @@
+﻿using Tu_Deuda.ApplicationDB;
+
+namespace Tu_Deuda.Data
+{
+    public class Connections
+    {
+        public static string urlSupabase()
+        {
+            var _dbContext = new Application_Context();
+            var db = _dbContext.DBApp.Find(1);
+            return db.UrlProyect;
+        }
+
+        public static string keySupabase()
+        {
+            var _dbContext = new Application_Context();
+            var db = _dbContext.DBApp.Find(1);
+            return db.KeyProyect;
+        }
+
+        public static string urlFirebase()
+        {
+            var _dbContext = new Application_Context();
+
+            var db = _dbContext.DBApp.Find(1);
+
+            return db.UrlProyect;
+        }
+
+        public static string urlWebApi()
+        {
+            var _dbContext = new Application_Context();
+            var db = _dbContext.DBApp.Find(1);
+
+            return db.UrlProyect;
+        }
+    }
+}
